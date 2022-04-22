ae9f3b4464dbf1c46f22a2081f75f1e2f4021719
bode <legionpath@gmail.com>
2022-04-22 18:31:06 +0900

    This is just a demo

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print('bode is my name')
+    print('makes changes')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
